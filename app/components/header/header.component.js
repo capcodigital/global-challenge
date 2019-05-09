ed7280ad416424cfaebd6d8aed45fbac656bbdfd
Chukwudi Anyiam-Osigwe <chukwudi.anyiam.osigwe@capco.com>
2019-05-09 13:42:27 +0100

    WIP: home page design

--- app/components/header/header.component.js
+++ app/components/header/header.component.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Icon } from 'semantic-ui-react';
-import { FormattedMessage } from 'react-intl';
-import Banner from './images/banner.png';
+import { Menu, Container } from 'semantic-ui-react';
+import Logo from './images/capco.png';
 import './style.scss';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className="header">
-        <img src={Banner} width="200px" alt="Logo" />
-        <div className="nav-bar">
-          <Link to="/">
-            <Button>
-              <Icon name="angle down" />
-              <FormattedMessage id="homepage.label" />
-            </Button>
-          </Link>
-        </div>
+        <Menu borderless secondary pointing size="small">
+          <Container>
+            <Menu.Item>
+              <Link to="/">
+                <img src={Logo} width="100px" alt="Logo" />
+              </Link>
+            </Menu.Item>
+          </Container>
+        </Menu>
       </div>
     );
   }
