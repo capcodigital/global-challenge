d7574afdd70d064b4abf3e7c4bb7b781cccf4115
David Stewart <david.stewart@capco.com>
2021-03-30 14:02:19 +0100

    SGCG-40: main view structure complete

--- app/components/common/list.component.js
+++ app/components/common/list.component.js
@@ -1,32 +1,42 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
-import React from 'react';
-import PropTypes from 'prop-types';
-import { List, Image } from 'semantic-ui-react';
-import { FormattedNumber } from 'react-intl';
-import './style.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import { List, Image } from "semantic-ui-react";
+import { FormattedNumber } from "react-intl";
+import "./style.scss";
 
-const ListView = ({
-  list, prefix, image, dataKey, className
-}) => (
+const ListView = ({ list, prefix, image, dataKey, className }) => (
   <div>
     <List animated divided className={className}>
       {list.map((item) => (
         <List.Item key={item.name}>
-          {image
-            ? <Image avatar src={require(`images/${item.name}.png`)} />
-            : ''}
+          {image ? (
+            <Image avatar src={require(`images/${item.name}.png`)} />
+          ) : (
+            ""
+          )}
           <List.Content>
             <List.Header>{item.name}</List.Header>
             <List.Description>
               {`${prefix} `}
-              <b><FormattedNumber value={dataKey ? item[dataKey] : item.steps} /></b>
+              <b>
+                <FormattedNumber value={dataKey ? item[dataKey] : item.steps} />
+              </b>
             </List.Description>
           </List.Content>
         </List.Item>
       ))}
     </List>
   </div>
+);
+export const ResizableListView = ({ className, height }) => (
+    <List
+      animated
+      divided
+      className={className}
+      style={{ height: height }}
+    />
 );
 
 ListView.propTypes = {
@@ -35,6 +45,11 @@ ListView.propTypes = {
   image: PropTypes.bool,
   prefix: PropTypes.string,
   className: PropTypes.string
+};
+
+ResizableListView.propTypes = {
+  className: PropTypes.string,
+  height: PropTypes.number,
 };
 
 export default ListView;
