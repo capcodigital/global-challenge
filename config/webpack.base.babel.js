54ff9a4c7e3e789b6650c9297d6f643193547801
David Stewart <david.stewart@capco.com>
2021-05-18 17:53:22 +0100

    Pass Google Maps API key to webpack on app build

--- config/webpack.base.babel.js
+++ config/webpack.base.babel.js
@@ -74,7 +74,8 @@ module.exports = (options) => ({
     // drop any unreachable code.
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        REACT_APP_GOOGLE_MAPS_API_KEY: JSON.stringify(process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
       }
     })
   ]),
