623e199940b516b112bac0a71f7efb4efff61cc3
Ivo Mladenov <ivo.mladenov@capco.com>
2021-06-16 13:33:51 +0100

    HTTPS changed to HTTP where missing

--- app/components/teams/teams.component.js
+++ app/components/teams/teams.component.js
@@ -10,7 +10,7 @@ import RegistrationForm from '../register';
 import Banner from './images/banner.png';
 import './style.scss';
 
-const SERVER_URL = process.env.SERVER_URL ? `https://${process.env.SERVER_URL}/` : 'https://localhost/';
+const SERVER_URL = process.env.SERVER_URL ? `https://${process.env.SERVER_URL}/` : 'http://localhost/';
 
 class TeamsPage extends React.Component {
   constructor(props) {
