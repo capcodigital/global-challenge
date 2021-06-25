757fab9f28cd7498e2ea365e1ede29d2d9adc4d0
David Stewart <david.stewart@capco.com>
2021-06-25 10:28:26 +0100

    Update boolean statement for checking max team size is five members

--- server/controllers/teams.controller.js
+++ server/controllers/teams.controller.js
@@ -187,7 +187,7 @@ exports.create = function(req, res) {
             res.send(400, { message: 'createTeamFailedTooFewPeople'});
             return;
         }
-        if (team.members.length > maxMembers) {
+        if (team.members.length >= maxMembers) {
             console.log("Max Fail");
             res.send(400, { message: 'createTeamFailedTooManyPeople'});
             return;
@@ -236,7 +236,7 @@ exports.update = function(req, res) {
                 res.send(400, { message: 'joinTeamFailedAlreadyAMember'});
                 return;
             }
-            if (team.members.length > maxMembers) {
+            if (team.members.length >= maxMembers) {
                 res.send(400, { message: 'joinTeamFailedTooManyPeople'});
                 return;
             }
