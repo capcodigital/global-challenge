c8d2d0646c5e9d8b8491e82f2acdeb4e53111bc2
David Stewart <david.stewart@capco.com>
2021-07-22 14:51:17 +0100

    Finalise styling for FAQ page

--- app/components/faq/FaqPage.component.js
+++ app/components/faq/FaqPage.component.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
-import { Header, Grid } from "semantic-ui-react";
+import { Header, Grid, Divider } from "semantic-ui-react";
 import QAndAPair from "./QAndAPair.component";
 import { questionData } from "./questionData";
 import "./style.scss";
@@ -17,18 +17,21 @@ function FaqPage() {
             <Header className="section-header">
               General
             </Header>
+            <Divider/>
             {questionData.general.map((question) => (
               <QAndAPair key={question.questionId} question={question} />
             ))}
             <Header className="section-header">
               Team
             </Header>
+            <Divider/>
             {questionData.team.map((question) => (
               <QAndAPair key={question.questionId} question={question} />
             ))}
             <Header className="section-header">
               Data
             </Header>
+            <Divider/>
             {questionData.data.map((question) => (
               <QAndAPair key={question.questionId} question={question} />
             ))}
