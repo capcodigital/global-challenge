b430ade026e955847c4fa621bb88f52f63dd9cbe
David Stewart <david.stewart@capco.com>
2021-10-17 18:59:24 +0100

    Update FAQ page

--- app/components/faq/FaqPage.component.js
+++ app/components/faq/FaqPage.component.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
-import { Header, Grid, Divider, Segment } from "semantic-ui-react";
+import { Header, Grid, Divider, Segment, Container } from "semantic-ui-react";
 import QAndAPair from "./QAndAPair.component";
 import { questionData } from "./questionData";
 import "./style.scss";
@@ -12,9 +12,12 @@ const challenge_name = process.env.CHALLENGE_NAME
 
 function FaqPage() {
   return (
-    <Segment className="secondary">
-      <Grid className={`faq-container ${challenge_name}`}>
-        <Grid.Column className={`faq-section column-b ${challenge_name}`}>
+    <Container>
+    <Segment vertical>
+      <Grid className={`faq-container ${challenge_name}`} stackable
+            centered>
+        <Grid.Column className={`faq-section ${challenge_name}`} style={{ paddingBottom: "2rem", paddingTop: "2rem" }}
+            width={16}>
           {challenge_name === "global" && (
             <>
               <Header className="faq-title">FAQ</Header>
@@ -23,7 +26,12 @@ function FaqPage() {
               {questionData.general.map((question) => (
                 <QAndAPair key={question.questionId} question={question} />
               ))}
-              <Header className="section-header">Team</Header>
+              <Header className="section-header">Tracking Activity</Header>
+              <Divider />
+              {questionData.trackingActivity.map((question) => (
+                <QAndAPair key={question.questionId} question={question} />
+              ))}
+              <Header className="section-header">Participating as a Team</Header>
               <Divider />
               {questionData.team.map((question) => (
                 <QAndAPair key={question.questionId} question={question} />
@@ -58,6 +66,7 @@ function FaqPage() {
         </Grid.Column>
       </Grid>
     </Segment>
+    </Container>
   );
 }
 
