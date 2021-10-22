40317f61937226a340bb411bccd97266c9cbfba6
Andrew Kentish <andrew.kentish@capco.com>
2021-10-22 08:42:43 +0100

    Minor code formatting tidy

--- server/controllers/strava.controller.js
+++ server/controllers/strava.controller.js
@@ -72,8 +72,8 @@ exports.authorize = function(req, res) {
                 console.log("Request Error: " + err);
                 res.redirect(callbackUrl + 'register?success=stravaError');
             } else if (result.errors && result.errors.length > 0) {
-                    console.log("Request Error: " + result.errors);
-                    res.redirect(callbackUrl + 'register?success=stravaError');
+                console.log("Request Error: " + result.errors);
+                res.redirect(callbackUrl + 'register?success=stravaError');
             } else {
                 console.log("Strava Result: " + result);
                 Capco.findOne({username: username.toUpperCase()}).exec(function(err, profile) {
