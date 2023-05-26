36904f479131d216d43dd78f8b477c7ba7ba2fc8
Andrew Kentish <andrew.kentish@capco.com>
2023-05-26 12:13:45 +0100

    Hard coded challenge dates

--- app/components/uk/common/Countdown.component.js
+++ app/components/uk/common/Countdown.component.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Button, Icon } from "semantic-ui-react";
 
-const challengeStartDate = new Date(2023, 3, 1, 0, 0, 0, 0); // 1 st March
-const challengeEndDate = new Date(2023, 3, 31, 23, 59, 59, 0); // 31st March
+const challengeStartDate = new Date(2023, 4, 26, 0, 0, 0, 0); // 1 st March
+const challengeEndDate = new Date(2023, 5, 2, 23, 59, 59, 0); // 31st March
 
 function getTimeRemaining() {
   let total = new Date();
