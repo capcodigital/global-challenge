cc327e718a252e4f500df4e2d5545b4bb3088b3b
Andrew Kentish <andrew.kentish@capco.com>
2023-07-05 11:44:08 +0100

    Updated email wording

--- server/controllers/fitbit.controller.js
+++ server/controllers/fitbit.controller.js
@@ -435,14 +435,16 @@ function save(user, res) {
             });
 
             /*
-            let emailText = "Hello " + user.name + ",\n\r You have successfully registered for the Capco Global Challenge with your FitBit account. \n\r" +
+            let emailText = "Hello " + user.name + ",\n\rYou have successfully registered for the Capco Global Challenge with your FitBit account. \n\r" +
                             "If you wish to create or join a team as part of the challenge, please go here: " + callbackUrl + "teams/register \n\r" +
                             "Once the challenge starts you can view your progress here: " + callbackUrl + "\n\r" +
-                            "Good Luck \n\r Capco Health & Wellbeing";
+                            "Good Luck \n\rCapco Health & Wellbeing";
             */
 
             let emailText = "Hello " + user.name + ",\n\rYou have successfully registered for the Capco Global Challenge with your FitBit account. \n\r" +
                             "Once the challenge starts you can view your progress here: " + callbackUrl + "\n\r" +
+                            "If you did not register or wish to be removed from the challenge and your account deleted please email the support team" +
+                            " challenge@capco.com\n\r" +
                             "Good Luck \n\rCapco Health & Wellbeing";
 
             mailer.sendMail(user.email, "Capco Challenge Registration Successfull", emailText, function() {
