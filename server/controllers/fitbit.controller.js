ac87d13904c0fe9811ebe41ef3fa32e1146654b4
Andrew Kentish <andrew.kentish@capco.com>
2024-06-12 09:32:32 +0100

    Email date fix

--- server/controllers/fitbit.controller.js
+++ server/controllers/fitbit.controller.js
@@ -164,7 +164,7 @@ exports.authorize = function(req, res) {
                                         */
 
                                         let emailText = "Hello " + user.name + ",\n\rYou have successfully registered for the Capco Global Challenge with your FitBit account. \n\r" +
-                                                        "Once the challenge starts on June 5th, you can view your progress here: " + callbackUrl + "\n\r" +
+                                                        "Once the challenge starts on June 26th, you can view your progress here: " + callbackUrl + "\n\r" +
                                                         "If you did not register or wish to be removed from the challenge and your account deleted please email the support team" +
                                                         " challenge@capco.com\n\r" +
                                                         "Thank you for your support - and good luck \n\rCapco Health & Wellbeing";
